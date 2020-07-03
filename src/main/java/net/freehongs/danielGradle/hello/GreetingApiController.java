0ad19591a5e235b84491c9f76e9fb3d05a1577f1
진형은 <hyungeun.jin@wadiz.kr>
2020-07-03 23:20:39 +0900

    Initial commit

---
+++ src/main/java/net/freehongs/danielGradle/hello/GreetingApiController.java
@@ -0,0 +1,31 @@
+package net.freehongs.danielGradle.hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Class GreetingApiController
+ * @Description
+ * @Author hyungeun.jin
+ * @Since 2020. 7. 3.
+ * @Version 1.0
+ * @COPYRIGHT © WADIZ ALL RIGHTS RESERVED.
+ * ------------------------------------------------------------------------
+ * Modification Information
+ * ------------------------------------------------------------------------
+ * 수정일 || 수정자 || 수정내용
+ * ------------------------------------------------------------------------
+ * 2020. 7. 3. || 진형은 || 최초생성
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/greeting/")
+public class GreetingApiController {
+
+  @GetMapping("/hello.do")
+  public String hello(){
+    return "hello springboot world";
+  }
+}
---
+++ src/test/java/net/freehongs/danielGradle/DanielGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.freehongs.danielGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielGradleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
