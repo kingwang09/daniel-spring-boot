efce085be20b0432e3cd5c5abfe46daf5cec7e04
진형은 <hyungeun.jin@wadiz.kr>
2021-02-16 13:08:39 +0900

    hongsnet 최신 버전으로

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-TOMCAT_HOME="/home2/google/tomcat/bin"
-HTML_HOME="/home2/google/public_html"
+TOMCAT_HOME="/home2/google/tomcat"
+HTML_HOME="/home2/google/tomcat/webapps/ROOT"
+FROM_DEPLOY_WAR="/home2/google/daniel-spring-boot-0.0.1-SNAPSHOT.war"
+TO_DEPLOY_WAR="/home2/google/tomcat/webapps/ROOT/daniel-spring-boot-0.0.1-SNAPSHOT.war"
 DEPLOY_WAR="daniel-spring-boot-0.0.1-SNAPSHOT.war"
 
 echo -e "\ntomcat stop..."
 cd $TOMCAT_HOME
-./catalina.sh stop
+./bin/catalina.sh stop
 
 echo -e "\nmoving deploy..."
-mv $DEPLOY_WAR $HTML_HOME
+mv $FROM_DEPLOY_WAR $HTML_HOME
 
 echo -e "\ndeploy war..."
 cd $HTML_HOME
-jar xvf $DEPLOY_WAR
+jar xvf $TO_DEPLOY_WAR
 
 echo -e "\nremove war..."
 rm -rf $DEPLOY_WAR
 
 echo -e "\ntomcat restart..."
 cd $TOMCAT_HOME
-./catalina.sh start
+./bin/catalina.sh start
